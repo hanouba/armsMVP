71f43a6b165bc897e59e70a11e0f118f7c92473f
HanSir <han_belief@163.com>
2019-12-17 18:01:35 +0800

    基本数据实现

---
+++ fullvideo/src/main/java/com/hansen/fullvideo/MyApp.java
@@ -0,0 +1,16 @@
+package com.hansen.fullvideo;
+
+import android.app.Application;
+
+/**
+ * @author HanN on 2019/12/17 17:16
+ * @email: 1356548475@qq.com
+ * @project armmvp
+ * @description:
+ * @updateuser:
+ * @updatedata: 2019/12/17 17:16
+ * @updateremark:
+ * @version: 2.1.67
+ */
+public class MyApp extends Application {
+}
